794623802ee2de5f70009f786ae397ea7d0b7cb0
marduklien <marduklien@gmail.com>
2024-04-29 14:13:37 +0800

    第14次改CDN List.js路徑

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ import './node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 // import './node_modules/list.js/dist/list.min.js';
 import './assets/js/dropdown.js';
+// import './assets/js/list.js';
 
 import './node_modules/litepicker/dist/js/main.js';
-import './node_modules/tom-select/dist/js/tom-select.base.min';
 import './node_modules/nouislider/dist/nouislider.min.mjs';
 
 import './assets/scss/all.scss';
