6b30f0965133534d2699e284c42bea77354091fc
marduklien <marduklien@gmail.com>
2024-04-29 14:07:49 +0800

    第13次改List.js路徑

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './node_modules/@tabler/core/dist/js/tabler.min.js';
 import './node_modules/bootstrap/dist/js/bootstrap.min.js';
 
+// import './node_modules/list.js/dist/list.min.js';
 import './assets/js/dropdown.js';
 
 import './node_modules/litepicker/dist/js/main.js';
