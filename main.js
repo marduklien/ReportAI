cce77ccc1bfe7aa844ccd5c72e00be020fa0ec39
marduklien <marduklien@gmail.com>
2024-05-04 21:36:09 +0800

    第16次下拉選單修正

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 //coreui
-import 'https://cdn.jsdelivr.net/npm/@coreui/coreui-pro@5.1.0-alpha.0/dist/js/coreui.min.js';
+//import 'https://cdn.jsdelivr.net/npm/@coreui/coreui-pro@5.1.0-alpha.0/dist/js/coreui.min.js';
+
 
 import "./node_modules/@tabler/core/dist/js/tabler.min.js";
 
 import "./node_modules/bootstrap/dist/js/bootstrap.min.js";
 import "./assets/js/dropdown.js";
+
 
 // import "./node_modules/@tabler/core/dist/libs/list.js/dist/list.min.js";
 import "./node_modules/@tabler/core/dist/libs/nouislider/dist/nouislider.min.js";
