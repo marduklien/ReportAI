7c853c5513763ca0782a934d55d1a177ebff7089
marduklien <marduklien@gmail.com>
2024-05-05 00:38:02 +0800

    第17次複選下拉選單與日期選擇器完成

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-//coreui
-//import 'https://cdn.jsdelivr.net/npm/@coreui/coreui-pro@5.1.0-alpha.0/dist/js/coreui.min.js';
-
-
 import "./node_modules/@tabler/core/dist/js/tabler.min.js";
 
 import "./node_modules/bootstrap/dist/js/bootstrap.min.js";
@@ -12,7 +8,5 @@ import "./assets/js/dropdown.js";
 import "./node_modules/@tabler/core/dist/libs/nouislider/dist/nouislider.min.js";
 import "./node_modules/@tabler/core/dist/libs/litepicker/dist/js/main.js";
 import "./node_modules/@tabler/core/dist/libs/tom-select/dist/js/tom-select.complete.min.js";
-
-
 
 import "./assets/scss/all.scss";
